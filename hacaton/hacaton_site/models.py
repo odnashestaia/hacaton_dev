a65466fbf5a0166ec575fd09fb64f43841e41cda
odna_shestaia <odna_shestaia@mail.com>
2023-12-21 16:06:24 +0300

    перевел базу

--- hacaton/hacaton_site/models.py
+++ hacaton/hacaton_site/models.py
@@ -8,23 +8,31 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     """Расширенная таблица юзеров"""
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    points = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = "Пользователи"
+        verbose_name_plural = "Пользователи"
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь из стандарта"
+    )
+    points = models.IntegerField(default=0, verbose_name="Очки")
 
     def __str__(self):
         return "Profile for user {}".format(self.user.username)
 
 
 class NameTest(models.Model):
-    """таблица названий тестов"""  # TODO Нужно переименовать
+    """таблица названий тестов"""
 
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Название теста"
+        verbose_name_plural = "Название тестов"
+
+    name = models.CharField(max_length=100, verbose_name="Название теста")
     img = models.ImageField(
         verbose_name="Превью теста", blank=True, upload_to="images/test/"
     )
-    user_done = models.ManyToManyField(
-        Student, verbose_name="Юзеры прошедшие тест", null=True
-    )
+    user_done = models.ManyToManyField(Student, verbose_name="Юзеры прошедшие тест")
 
     def __str__(self) -> str:
         return self.name
@@ -37,14 +45,19 @@ class Article(models.Model):
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
 
-    title = models.CharField()
-    text = models.TextField()
-    points = models.IntegerField()
+    title = models.CharField(verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст статьи")
+    points = models.IntegerField(
+        default=0, verbose_name="Очки за прочтение статьи (не работает)"
+    )
     img = models.ImageField(
         verbose_name="Превью статьи", blank=True, upload_to="images/article/"
     )
     name_test = models.ForeignKey(
-        NameTest, related_name="article_category", on_delete=models.CASCADE
+        NameTest,
+        related_name="article_category",
+        on_delete=models.CASCADE,
+        verbose_name="Имя теста",
     )
 
     def __str__(self):
@@ -57,10 +70,17 @@ class Article(models.Model):
 class Question(models.Model):
     """вопросы - ссылаются на имена"""
 
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
     name_test = models.ForeignKey(
-        NameTest, related_name="question_test", on_delete=models.CASCADE
+        NameTest,
+        related_name="question_test",
+        on_delete=models.CASCADE,
+        verbose_name="Название теста",
     )
-    question = models.CharField(max_length=100)
+    question = models.CharField(max_length=100, verbose_name="Вопрос")
     marks = models.IntegerField(default=5)
 
     def __str__(self) -> str:
@@ -80,11 +100,18 @@ class Question(models.Model):
 class Answer(models.Model):
     """ответы - ссылаються на вопросы в обе стороны"""
 
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
     question = models.ForeignKey(
-        Question, related_name="question_answer", on_delete=models.CASCADE
+        Question,
+        related_name="question_answer",
+        on_delete=models.CASCADE,
+        verbose_name="Вопрос",
     )
-    answer = models.CharField(max_length=100)
-    is_correct = models.BooleanField(default=False)
+    answer = models.CharField(max_length=100, verbose_name="Ответ")
+    is_correct = models.BooleanField(default=False, verbose_name="Правильный ответ")
 
     def __str__(self) -> str:
         return self.answer
